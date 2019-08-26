6da40e10c132986012d44cb16b7f50b0aad2aea5
Vikash Kumar <vikash.kumar@Eternus.local>
2019-08-26 19:24:27 +0530

    a

--- startup/db.js
+++ startup/db.js
@@ -2,8 +2,8 @@ const mongoose=require('mongoose');
 const config=require('config');
 const winston=require('winston');
 module.exports=function(){
-    const db=config.get('db');
+    
     mongoose.connect("mongodb+srv://vikash:espl123@myfirstcluster-bhteb.mongodb.net/nodeproject?retryWrites=true&w=majority",{useNewUrlParser:true})
-    .then(()=>winston.info(`Connected to ${db}...`));
+    .then(()=>winston.info(`Connected to mongo...`));
 };
 
